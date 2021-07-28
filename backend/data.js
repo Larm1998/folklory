a64486505993641df84c51d05d1b7679220a6c76
Luis Rangel <luis18rangelm@gmail.com>
2021-07-27 19:36:36 -0500

    Initial Commit

---
+++ backend/data.js
@@ -0,0 +1,88 @@
+import bcrypt from 'bcryptjs';
+const data = {
+    users: [
+        {
+            name: 'Luis',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'John',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false,
+        },
+
+    ],
+    products:[
+        {
+            name: 'Tembleque',
+            category: 'Accesory',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'PA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name:'Peineton',
+            category:'Accesory',
+            image:'/images/p2.jpg',
+            price:100,
+            countInStock: 0,
+            brand:'PA',
+            rating:4.0,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            name:'Aretes',
+            category:'Accesory',
+            image:'/images/p3.jpg',
+            price:220,
+            countInStock: 1,
+            brand:'PA',
+            rating:4.8,
+            numReviews:17,
+            description:'high quality product',
+        },
+        {
+            name:'Pulsera',
+            category:'Accesory',
+            image:'/images/p4.jpg',
+            price:78,
+            countInStock: 12,
+            brand:'PA',
+            rating:4.5,
+            numReviews:14,
+            description:'high quality product',
+        },
+        {
+            name:'Rosario',
+            category:'Accesory',
+            image:'/images/p5.jpg',
+            price:65,
+            countInStock: 5,
+            brand:'PA',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product',
+        },
+        {
+            name:'Cadena Solitaria',
+            category:'Accesory',
+            image:'/images/p6.jpg',
+            price:139,
+            countInStock: 20,
+            brand:'PA',
+            rating:4.0,
+            numReviews:15,
+            description:'high quality product',
+        },
+    ],
+};
+
+export default data;
